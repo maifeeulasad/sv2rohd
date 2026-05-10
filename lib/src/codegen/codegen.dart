e5f3ee12708c3caa7c59593f7adf972921206dc9
Maifee Ul Asad <maifeeulasad@gmail.com>
2026-05-10 12:56:24 +0600

    [feat]: analysis, code gen;   - Symbol table and scope management   - Type checking and width propagation   - Expression evaluation and constant folding   - Symbol definitions and types   - IR builder and ROHD translator   - Scope management for hierarchical resolution   - Main ROHD code generator   - Expression to ROHD translation   - Signal declaration generation   - Module class generation
    File: lib/src/codegen/codegen.dart

---
+++ lib/src/codegen/codegen.dart
@@ -0,0 +1,7 @@
+// Copyright (C) 2026
+// SPDX-License-Identifier: BSD-3-Clause
+
+export 'rohd_generator.dart';
+export 'expression_generator.dart';
+export 'signal_generator.dart';
+export 'module_generator.dart';
