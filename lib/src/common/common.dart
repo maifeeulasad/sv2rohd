308c1053ff34a4f5424eaf30614898acb1d17851
Maifee Ul Asad <maifeeulasad@gmail.com>
2026-05-10 12:50:00 +0600

    [feat]: util, grammar, frontend, ir, test;   - Common utilities (diagnostics, naming strategy, source location)   - ANTLR4 grammar for SystemVerilog (lexer and parser)   - Frontend components (preprocessor, lexer/parser adapters)   - IR definitions (nodes, expressions, statements, modules)   - IR builder and ROHD translator   - Unit tests for common utilities and IR
    File: lib/src/common/common.dart

---
+++ lib/src/common/common.dart
@@ -0,0 +1,12 @@
+// Copyright (C) 2026
+// SPDX-License-Identifier: BSD-3-Clause
+
+/// Common utilities used throughout SV2ROHD.
+library sv2rohd.common;
+
+export 'source_location.dart';
+export 'source_range.dart';
+export 'diagnostic.dart';
+export 'diagnostic_collector.dart';
+export 'naming_strategy.dart';
+export 'utils.dart';
