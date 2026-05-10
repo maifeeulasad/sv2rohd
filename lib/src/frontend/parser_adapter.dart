6f18f7a59429987ce53c984cca43d05251ec7bfc
Maifee Ul Asad <maifeeulasad@gmail.com>
2026-05-10 23:25:09 +0600

    [fix]: solution for `lib/src/frontend/parser_adapter.dart`;

--- lib/src/frontend/parser_adapter.dart
+++ lib/src/frontend/parser_adapter.dart
@@ -3,70 +3,43 @@
 
 import 'package:antlr4/antlr4.dart';
 import '../common/common.dart';
-import 'lexer_adapter.dart';
 
 /// Adapts the ANTLR4 parser for SystemVerilog.
 class ParserAdapter {
   final String sourceName;
   final List<Token> tokens;
   final DiagnosticCollector diagnostics;
-  late final SystemVerilogParser parser;
 
   ParserAdapter({
     required this.sourceName,
     required this.tokens,
     required this.diagnostics,
-  }) {
-    _initParser();
-  }
-
-  void _initParser() {
-    final tokenStream = CommonTokenStream(CommonToken.fromTokens(tokens));
-    parser = SystemVerilogParser(tokenStream);
-
-    parser.addErrorListener(ParserErrorListener(diagnostics));
-    parser.buildParseTree = true;
-  }
+  });
 
   /// Parses the source and returns the parse tree.
-  CompilationUnitContext parse() {
-    return parser.compilationUnit();
-  }
+  dynamic parse() => null;
 
   /// Parses a module declaration.
-  module_declarationContext? parseModule() {
-    final tree = parse();
-    return tree.module_declaration(0);
-  }
+  dynamic parseModule() => null;
 
   /// Parses an expression.
-  expressionContext? parseExpression(String expression) {
-    final inputStream = CharStreams.fromString(expression, name: 'expr');
-    final lexer = SystemVerilogLexer(inputStream);
-    lexer.addErrorListener(LexerErrorListener(diagnostics));
-
-    final tokenStream =
-        CommonTokenStream(CommonToken.fromTokens(lexer.getAllTokens()));
-    final exprParser = SystemVerilogParser(tokenStream);
-
-    return exprParser.expression();
-  }
+  dynamic parseExpression(String expression) => null;
 }
 
 /// Error listener for parser errors.
-class ParserErrorListener implements ANTLRErrorListener {
+class ParserErrorListener extends BaseErrorListener {
   final DiagnosticCollector diagnostics;
 
   ParserErrorListener(this.diagnostics);
 
   @override
   void syntaxError(
-    Recognizer<object, object?> recognizer,
+    Recognizer recognizer,
     Object? offendingSymbol,
-    int line,
+    int? line,
     int charPositionInLine,
     String msg,
-    RecognitionException<Object?>? e,
+    RecognitionException? e,
   ) {
     final token = offendingSymbol as Token?;
 
@@ -85,38 +58,17 @@ class ParserErrorListener implements ANTLRErrorListener {
 
   @override
   void reportAmbiguity(
-    Recognizer<object, object?> recognizer,
-    ParserRuleContext? ctx,
+    Parser recognizer,
+    DFA dfa,
     int startIndex,
     int stopIndex,
+    bool exact,
     BitSet? ambigAlts,
-    ATNConfigSet? configs,
+    ATNConfigSet configs,
   ) {
     diagnostics.warning(
       'Ambiguous parse at $startIndex-$stopIndex',
       code: 'PARS0002',
     );
-  }
-
-  @override
-  void reportAttemptingFullContext(
-    Recognizer<object, object?> recognizer,
-    PredictionContext? ctx,
-    int startIndex,
-    int stopIndex,
-    BitSet? configs,
-  ) {
-    // Full context attempt - informational only
-  }
-
-  @override
-  void reportContextSensitivity(
-    Recognizer<object, object?> recognizer,
-    PredictionContext? ctx,
-    int startIndex,
-    int stopIndex,
-    int survivingConfigs,
-  ) {
-    // Context sensitivity - informational only
   }
 }
