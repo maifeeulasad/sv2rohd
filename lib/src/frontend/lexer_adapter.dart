0505a767be7cbc9f1c56dac1c82886ca1e64f0f0
Maifee Ul Asad <maifeeulasad@gmail.com>
2026-05-10 23:25:09 +0600

    [fix]: solution for `lib/src/frontend/lexer_adapter.dart`;

--- lib/src/frontend/lexer_adapter.dart
+++ lib/src/frontend/lexer_adapter.dart
@@ -3,66 +3,8 @@
 
 import 'package:antlr4/antlr4.dart';
 
-/// Represents preprocessed source text with source mapping.
-class SourceText {
-  final String sourceName;
-  final String text;
-  final SourceMap sourceMap;
-
-  const SourceText(
-    this.sourceName,
-    this.text, {
-    this.sourceMap = const _EmptySourceMap(),
-  });
-
-  /// Returns the length of the source text.
-  int get length => text.length;
-
-  /// Returns the source location for a given offset.
-  SourceLocation getLocation(int offset) {
-    if (offset < 0 || offset > text.length) {
-      return SourceLocation(sourceName, 0, 0, offset);
-    }
-
-    int line = 1;
-    int column = 1;
-    int currentOffset = 0;
-
-    for (int i = 0; i < text.length && currentOffset < offset; i++) {
-      if (text[i] == '\n') {
-        line++;
-        column = 1;
-      } else {
-        column++;
-      }
-      currentOffset++;
-    }
-
-    return SourceLocation(sourceName, line, column, offset);
-  }
-}
-
-class _EmptySourceMap implements SourceMap {
-  const _EmptySourceMap();
-
-  @override
-  void addMapping(int preprocessedOffset, int originalOffset) {}
-
-  @override
-  int? toOriginalOffset(int preprocessedOffset) => preprocessedOffset;
-
-  @override
-  List<int> get preprocessedOffsets => [];
-
-  @override
-  void clear() {}
-
-  @override
-  int get length => 0;
-
-  @override
-  bool get isEmpty => true;
-}
+import '../common/common.dart';
+import 'source_text.dart';
 
 /// Adapts the ANTLR4 lexer for SystemVerilog.
 class LexerAdapter {
@@ -111,61 +53,23 @@ class LexerAdapter {
 }
 
 /// Error listener for lexer errors.
-class LexerErrorListener implements ANTLRErrorListener {
+class LexerErrorListener extends BaseErrorListener {
   final DiagnosticCollector diagnostics;
 
   LexerErrorListener(this.diagnostics);
 
   @override
   void syntaxError(
-    Recognizer<object, object?> recognizer,
+    Recognizer recognizer,
     Object? offendingSymbol,
-    int line,
-    int charPositionInLine,
+    int? line,
+    int? charPositionInLine,
     String msg,
-    RecognitionException<Object?>? e,
+    RecognitionException? e,
   ) {
     diagnostics.error(
-      'Lexer error at line $line:$charPositionInLine: $msg',
+      'Lexer error at line ${line ?? 0}:${charPositionInLine ?? 0}: $msg',
       code: 'LEX0001',
     );
-  }
-
-  @override
-  void reportAmbiguity(
-    Recognizer<object, object?> recognizer,
-    ParserRuleContext? ctx,
-    int startIndex,
-    int stopIndex,
-    BitSet? ambigAlts,
-    ATNConfigSet? configs,
-  ) {
-    // Report ambiguity as warning
-    diagnostics.warning(
-      'Ambiguous token at $startIndex-$stopIndex',
-      code: 'LEX0002',
-    );
-  }
-
-  @override
-  void reportAttemptingFullContext(
-    Recognizer<object, object?> recognizer,
-    PredictionContext? ctx,
-    int startIndex,
-    int stopIndex,
-    BitSet? configs,
-  ) {
-    // Full context attempt - informational only
-  }
-
-  @override
-  void reportContextSensitivity(
-    Recognizer<object, object?> recognizer,
-    PredictionContext? ctx,
-    int startIndex,
-    int stopIndex,
-    int survivingConfigs,
-  ) {
-    // Context sensitivity - informational only
   }
 }
